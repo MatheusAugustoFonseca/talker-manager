ff26a2a7a3c9398727fd6296275fe224371b0774
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-11-29 10:43:41 -0300

    req 2 - criando endpoint /talker/:id - Done

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -15,6 +16,18 @@ app.get('/', (_request, response) => {
 
 app.get('/talker', async (_req, res) => {
   res.status(HTTP_OK_STATUS).json(await readFile());
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readFile();
+  // const specifcTalker = talkers.find(({ id }) => id === Number(id));
+  const specifcTalker = talkers.find((talker) => talker.id === Number(id));
+  if (specifcTalker) {
+    res.status(HTTP_OK_STATUS).json(specifcTalker);
+  } else {
+    res.status(HTTP_NOT_FOUND_STATUS).send({ message: 'Pessoa palestrante não encontrada' });
+  }
 });
 
 app.listen(PORT, () => {
